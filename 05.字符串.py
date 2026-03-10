5537bdcd6f1856934dbb885a219164acc764d31c
sky-seven183 <3157014909@qq.com>
2026-03-10 21:50:25 +0800

    Add files via upload
    day02-第一章

---
+++ 05.字符串.py
@@ -0,0 +1,28 @@
+#双引号定义
+s1 = "Hello"#每个字母、数字、汉字都是字符
+
+#单引号定义
+s2 = 'Python'   #项目中保持一种写法即可
+
+#三引号定义
+s3 = """
+
+南京，我来了：
+    你林，你绝对完了，
+    擦干净，等着我吧！
+    """
+
+
+print(s1)
+print(s2)
+print(s3)
+
+print(type(s1))
+print(type(s2))
+print(type(s3))
+
+#定义字符串---> It's a good boy.
+#使用转义字符  \'(单引号)  \"(双引号)  \n 换行  \t 制表符
+s4 = 'It\'s awesome!'
+print(s4)
+print("你林来了\n什么?奶龙来了！")
